69ce36f81125bc7375ce40d26db3663e8cad618c
Kinner <kinner@outlook.com>
2021-09-30 23:25:04 -0400

    lab4 work in progress

--- Labs/LAB4.py
+++ Labs/LAB4.py
@@ -34,7 +34,7 @@ class SortedLinkedList:
         >>> x.replicate()
         Head:Node(-7.5)
         Tail:Node(9.78)
-        List:-7.5 -> 1 -> 1 -> 1 -> 3 -> 3 -> 3 -> 4 -> 4 -> 4 -> 4 -> 4 -> 4 -> 4 -> 4 -> 5 -> 5 -> 5 -> 5 -> 5 -> 8.76 -> 9.78
+        List:-7.5 -> -7.5 -> 1 -> 1 -> 1 -> 3 -> 3 -> 3 -> 4 -> 4 -> 4 -> 4 -> 4 -> 4 -> 4 -> 4 -> 5 -> 5 -> 5 -> 5 -> 5 -> 8.76 -> 8.76 -> 9.78 -> 9.78
         >>> x
         Head:Node(-7.5)
         Tail:Node(9.78)
@@ -75,32 +75,47 @@ class SortedLinkedList:
 
                 
     def add(self, value):
+        newNode = Node(value)
         if self.head is None:
-            self.head = Node(value)
-            self.tail = self.head
+            self.head = newNode
+            self.tail = newNode
+        elif value <= self.head.value:
+            newNode.next = self.head
+            self.head = newNode
+        elif value >= self.tail.value:
+            self.tail.next = newNode
+            self.tail = self.tail.next
         else:
             curr = self.head
-            while curr.value < value and curr.next is not self.tail:
+            while curr.next is not None and curr.next.value <= value:
                 curr = curr.next
-            if curr.next is self.tail:
-                self.tail.next = Node(value)
-                self.tail = self.tail.next
-            elif curr is self.head:
-                temp = self.head
-                self.head = Node(value)
-                self.head.next = temp
-            else:
-                temp = 
+            newNode.next = curr.next
+            curr.next = newNode
             
 
     def replicate(self):
-        # --- YOUR CODE STARTS HERE
-        pass
+        newList = self
+        if newList.head is None:
+            return None
+        curr = newList.head
+        while curr is not None:
+            repeat = curr.value - 1
+            if isinstance(curr.value, float) or curr.value <= 0:
+                repeat = 1
+            for i in range(0, repeat):
+                self.add(curr.value)
+                curr = curr.next
+            curr = curr.next
+        
+        return newList
 
 
     def removeDuplicates(self):
-        # --- YOUR CODE STARTS HERE
-        pass
+        curr = self.head
+        while curr is not None:
+            while curr.next is not None and curr.value == curr.next.value:
+                curr.next = curr.next.next
+            curr = curr.next
 
 if __name__=='__main__':
     import doctest
